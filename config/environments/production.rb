7260646facc73991ecfb34efb70ace144fa37068
hrmn22 <harman@bluebash.co>
2022-11-25 12:16:22 +0530

    change the compresser

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.css_compressor = :yui
-config.assets.js_compressor = :terser 
+config.assets.js_compressor = :uglifier
 config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => '5fd9db6a2119c4',

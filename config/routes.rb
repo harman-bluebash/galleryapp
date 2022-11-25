3a357ae81a6167ce75f40faeff908050f504eb7b
hrmn22 <harman@bluebash.co>
2022-11-25 17:46:43 +0530

    given the route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
  
   devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
   root"home#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
